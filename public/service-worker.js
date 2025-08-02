f15617b5853e28c7cab180e3e95db1e85ac7590d
Paweł Mucha <pawel@mucha.com>
2025-08-02 09:08:02 +0200

    Added simple support for online-first PWA

---
+++ public/service-worker.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('quiz-game-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open('quiz-game-cache-v1').then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
